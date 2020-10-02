e8194cc2d72c58fbae78351a85931a3a7589d12b
abhinavshinow <55484189+alphabeta2206@users.noreply.github.com>
2020-10-02 13:15:35 +0530

    Animated qr code finder in progress

--- esep_client/lib/routes/qrscanpage.dart
+++ esep_client/lib/routes/qrscanpage.dart
@@ -1,29 +1,91 @@
 import 'package:flutter/material.dart';
-import '../partials/custom_textfield.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-import 'package:flutter_qr_bar_scanner/flutter_qr_bar_scanner.dart';
+import 'package:flutter_qr_bar_scanner/qr_bar_scanner_camera.dart';
+import 'package:animated_qr_code_scanner/animated_qr_code_scanner.dart';
 
-class Scan extends StatefulWidget {
-  @override
-  _ScanState createState() => _ScanState();
-}
-class _ScanState extends State<Scan> {
-
-
-
+class Scan extends StatelessWidget {
+  final AnimatedQRViewController controller = AnimatedQRViewController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      toolbarHeight: 90.0,
-      title: Text(
-        "Scan QR Code",
-        style: const TextStyle(
-          fontSize: 28.0,
-          color: Colors.blueGrey,
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: AnimatedQRView(
+              squareColor: Colors.green.withOpacity(0.25),
+              animationDuration: const Duration(milliseconds: 400),
+              onScanBeforeAnimation: (String str) {
+                print('Callback at the beginning of animation: $str');
+              },
+              onScan: (String str) async {
+                await showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text("Callback at the end of animation: $str"),
+                    actions: [
+                      FlatButton(
+                        child: Text('OK'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          Navigator.of(context).pop();
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => Scaffold(
+                                body: Align(
+                                  alignment: Alignment.center,
+                                  child: Text("$str"),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                      FlatButton(
+                        child: Text('Rescan'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          controller.resume();
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+              controller: controller,
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.blue,
+                  child: Text('Flash'),
+                  onPressed: () {
+                    controller.toggleFlash();
+                  },
+                ),
+                const SizedBox(width: 10),
+                FlatButton(
+                  color: Colors.blue,
+                  child: Text('Flip'),
+                  onPressed: () {
+                    controller.flipCamera();
+                  },
+                ),
+                const SizedBox(width: 10),
+                FlatButton(
+                  color: Colors.blue,
+                  child: Text('Resume'),
+                  onPressed: () {
+                    controller.resume();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-    ));
+    );
   }
 }
